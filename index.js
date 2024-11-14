135cc19b10acc3794a9c683c264894ccda2edffe
Marc Totaro <m.totaro@tecplot.com>
2024-11-13 21:05:22 -0600

    feat: outlined api points

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +8,25 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("api/shorturl", function (req, res) {
+  return null; // code here for sending the url to db and getting id for json response
+});
+
+app.get("api/shorturl/:id", function (req, res) {
+  return null; // find in db the matching id and show the matching url in json
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
