0755b2d5825aab8e63f5b6db1de7c431ee591916
Marc <marctotaro94@gmail.com>
2024-11-13 22:45:24 -0600

    feat: send url to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const dns = require("node:dns");
+const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require('mongoose')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,8 +22,59 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-app.post("api/shorturl", function (req, res) {
-  return null; // code here for sending the url to db and getting id for json response
+// TODO: setup mongodb connection
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: process.env.MONGO_DB_NAME
+    });
+// setup schema
+const urlSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+  short_url: {
+    type: Number,
+    required: true,
+  }
+});
+// setup model
+let ShortURL = mongoose.model('short_url', urlSchema);
+
+const createAndSaveShortURL = (original_url, done) => {
+  ShortURL.create({
+    url: original_url,
+    short_url: 2
+  }, function(err, data) {
+    done(null, data);
+  });
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/api/shorturl", function (req, res) {
+  dns.lookup(req.body.url, function (err, data) {
+    console.log(data); // DNS resolved IP address
+    if (!err) {
+      // get the last entry from mongodb for id
+      // increment id
+      // post new url to mongodb, { id: Number, url: "String" }
+      // TODO: place DB id in the short_url response.
+      createAndSaveShortURL(req.body.url, (err, data) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(data);
+        res.json({"original_url": req.body.url, "short_url": data.short_url});
+      });
+    } else {
+      res.json( {"error": "invalid url" });
+    }
+  });
 });
 
 app.get("api/shorturl/:id", function (req, res) {
